cd6db1b7c64638a8f05144a7238ef28efd77ee9b
cherki-hamza <cherki0hamza@gmail.com>
2024-08-03 21:38:02 +0400

    update nodejs app

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const port = 3000;
 
 app.use(cors());
 
+app.get("/" , (req,res) => {
+  res.writeHead(200 , {"Content-Type":"text/html"});
+  res.end(`<center><h1 style='color:red;margin-top:100px'>hello from cherki hamza developer web fullstack</h1></center>`);
+});
+
 app.get("/api/data",(req,res)=>{
 
     const data = {
